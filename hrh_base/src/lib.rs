ab4533fd5dbac5b3cab53f1e1ab39f6942638dd1
Beetroot Paul <paul@beetrootpaul.com>
2023-04-04 12:24:16 +0200

    initial bevy_pixels integration

--- hrh_base/src/lib.rs
+++ hrh_base/src/lib.rs
@@ -1,12 +1,16 @@
 extern crate bevy;
+extern crate bevy_pixels;
 
-pub struct HrhWindowConfig {
+use bevy::math::UVec2;
+use bevy::prelude::IntoSystemConfig;
+
+pub struct HrhBaseConfig {
     pub title: String,
-    pub logical_width: i32,
-    pub logical_height: i32,
+    pub canvas_size: UVec2,
+    pub initial_canvas_zoom: u32,
 }
 
-pub fn new_hrh_base_bevy_app(config: HrhWindowConfig) -> bevy::prelude::App {
+pub fn new_hrh_base_bevy_app(config: HrhBaseConfig) -> bevy::prelude::App {
     let mut app = bevy::app::App::new();
 
     app.add_plugins(bevy::MinimalPlugins);
@@ -23,8 +27,8 @@ pub fn new_hrh_base_bevy_app(config: HrhWindowConfig) -> bevy::prelude::App {
         primary_window: Some(bevy::window::Window {
             title: config.title,
             resolution: bevy::window::WindowResolution::new(
-                config.logical_width as f32,
-                config.logical_height as f32,
+                (config.canvas_size.x * config.initial_canvas_zoom) as f32,
+                (config.canvas_size.y * config.initial_canvas_zoom) as f32,
             ),
             ..bevy::utils::default()
         }),
@@ -38,5 +42,29 @@ pub fn new_hrh_base_bevy_app(config: HrhWindowConfig) -> bevy::prelude::App {
     #[cfg(debug_assertions)]
     app.add_system(bevy::window::close_on_esc);
 
+    // https://crates.io/crates/bevy_pixels
+    app.add_plugin(bevy_pixels::PixelsPlugin {
+        primary_window: Some(bevy_pixels::PixelsOptions {
+            width: config.canvas_size.x,
+            height: config.canvas_size.y,
+            auto_resize_surface: false,
+            auto_resize_buffer: false,
+            // value below is not used, because of auto_resize_buffer set to false above
+            scale_factor: 1.0,
+        }),
+    });
+    app.add_system(draw.in_set(bevy_pixels::PixelsSet::Draw));
+
     app
+}
+
+fn draw(mut wrapper_query: bevy::prelude::Query<&mut bevy_pixels::PixelsWrapper>) {
+    for mut wrapper in &mut wrapper_query {
+        let frame = wrapper.pixels.frame_mut();
+        let f_len = frame.len();
+        frame[0..(f_len / 2)]
+            .copy_from_slice(&[0x48, 0xb2, 0xe8, 0xff, 0x23, 0xe2, 0x78, 0xff].repeat(f_len / 16));
+        frame[(f_len / 2)..f_len]
+            .copy_from_slice(&[0xf5, 0xb2, 0x12, 0xff, 0xd4, 0xe2, 0x33, 0xff].repeat(f_len / 16));
+    }
 }
--- hrh_game/src/lib.rs
+++ hrh_game/src/lib.rs
@@ -1,15 +1,21 @@
+extern crate bevy;
 extern crate hrh_base;
 
-use hrh_base::{new_hrh_base_bevy_app, HrhWindowConfig};
+use std::ops::Mul;
 
-const GAME_W: i32 = 560;
-const GAME_H: i32 = 432;
-const WINDOW_SCALE: i32 = 2;
+use bevy::math::{ivec2, uvec2, IVec2, UVec2};
+
+use hrh_base::{new_hrh_base_bevy_app, HrhBaseConfig};
+
+const TILE_SIZE: UVec2 = uvec2(16, 16);
+const CANVAS_TILES_LANDSCAPE: UVec2 = uvec2(20, 12);
+const CANVAS_TILES_PORTRAIT: UVec2 = uvec2(12, 18);
+const INITIAL_CANVAS_ZOOM: u32 = 3;
 
 pub fn new_hrh_game() -> bevy::prelude::App {
-    new_hrh_base_bevy_app(HrhWindowConfig {
+    new_hrh_base_bevy_app(HrhBaseConfig {
         title: "Hen Rescue Hero".to_string(),
-        logical_width: GAME_W * WINDOW_SCALE,
-        logical_height: GAME_H * WINDOW_SCALE,
+        canvas_size: CANVAS_TILES_LANDSCAPE * TILE_SIZE,
+        initial_canvas_zoom: INITIAL_CANVAS_ZOOM,
     })
 }
